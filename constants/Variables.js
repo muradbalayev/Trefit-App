baaeb90dfcbcc195f53180fe4b3555634ce15516
mouradbalayev <muradbalazade@gmail.com>
2025-10-28 12:51:39 +0400

    build configuration fixed

--- constants/Variables.js
+++ constants/Variables.js
@@ -1,5 +1,6 @@
 const API_URL = process.env.EXPO_PUBLIC_API_URL 
 const IMAGE_URL= process.env.EXPO_PUBLIC_IMAGE_URL ;
-
+console.log("API_URL:", process.env.EXPO_PUBLIC_API_URL);
+console.log("IMAGE_URL:", process.env.EXPO_PUBLIC_IMAGE_URL);
 
 export {API_URL, IMAGE_URL}
